373d63ade0ab926d58917b72018a0bd1cc7c8a68
Sivaldo Almeida <sivaldovieiradealmeida@gmail.com>
2026-02-22 21:34:52 -0300

    feat: implementa dashboard de estoque critico e corrige nomes de campos

--- api/views.py
+++ api/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from produtos.models import Produto, Categoria
 from .serializers import ProdutoSerializer, CategoriaSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class ProdutoViewSet(viewsets.ModelViewSet):
     queryset = Produto.objects.all()
@@ -11,6 +13,16 @@ class ProdutoViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['categoria'] 
     search_fields = ['nome', 'sku']
+
+    @action(detail=False, methods=['get'])
+    def estoque_baixo(self, request):
+        """
+        Retorna apenas os produtos com menos de 10 unidades.
+        """
+        produtos = Produto.objects.filter(quantidade__lt=10) # __lt significa 'Less Than' (Menor que)
+        serializer = self.get_serializer(produtos, many=True)
+        return Response(serializer.data)
+    
 
 class CategoriaViewSet(viewsets.ModelViewSet):
     queryset = Categoria.objects.all()
--- produtos/views.py
+++ produtos/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Produto
 
-# Create your views here.
+def dashboard_estoque(request):
+    # Mudamos 'quantidade' para 'quantidade_estoque'
+    estoque_critico = Produto.objects.filter(quantidade_estoque__lt=10)
+    
+    total_produtos = Produto.objects.count()
+    # Mudamos aqui também para contar quem tem estoque
+    produtos_ativos = Produto.objects.filter(quantidade_estoque__gt=0).count()
+    
+    context = {
+        'produtos_alerta': estoque_critico,
+        'total_geral': total_produtos,
+        'em_estoque': produtos_ativos,
+        'alerta_count': estoque_critico.count(),
+    }
+    return render(request, 'dashboard.html', context)
